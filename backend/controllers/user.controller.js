bd86dc7c0c51ceb35b1762cb465151b9f5db7423
CasManny <chukwusabastine2018@gmail.com>
2024-11-14 14:02:46 +0100

    get users and stats

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,3 +1,13 @@
-export const getUser = async (req, res) => {
-    res.send('hello')
-}
+import { User } from "../models/user.model.js";
+
+export const getAllUsers = async (req, res, next) => {
+  try {
+    const currentUserId = req.auth.userId;
+    const users = await User.find({ clerkId: { $ne: currentUserId } });
+    res.status(200).json(users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+
